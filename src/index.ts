d8832010933f7793b3608b91ba54b2a7a533c6a3
Shunya <wisteria.shunya@gmail.com>
2025-05-25 10:59:28 +0900

    Add : abstract作成処理

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,145 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface Env {
+	LLM_API_ENDPOINT:string;
+	LLM_API_KEY:string;
+}
+
+export interface ReqBody {
+	entryId: string;
+	entryBody: string;
+	useCache: boolean;
+	saveCache: boolean;
+}
+
+export interface ResBody {
+	status: string;
+	code: string;
+	abstract: string;
+	useCache: boolean;
+	saveCache: boolean;
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request, env:Env, ctx): Promise<Response> {
+		const PronptBase: string = "下記ブログの記事を200字以内に要約してしてください。<p>タグでくくったhtmlをテキスト形式で出力し、markdownのcode blockは使用しないでください。\n";
+
+		// リクエストメソッドがPOSTでない場合はエラーを返す
+		if (request.method !== 'POST') {
+			const res: ResBody = {
+				status: "E",
+				code: "999",
+				abstract: "",
+				useCache: false,
+				saveCache: false
+			}
+
+			return new Response(JSON.stringify(res), {
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+					"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+					"Content-Type": "application/json; charset=utf-8"
+				}
+			});
+		}
+
+		else {
+			const reqBody: ReqBody = await request.json();
+
+			// キャッシュを使用する場合
+			if (reqBody.useCache) {
+				// KVを検索
+
+			}
+
+			// キャッシュを使用しない場合、Gemini APIを呼び出す
+			const abstract: string = await CreateAbstract(reqBody, env);
+			// APIエラーの場合終了
+			if(abstract === "") {
+				const res: ResBody = {
+				status: "E",
+				code: "900",
+				abstract: "",
+				useCache: false,
+				saveCache: false
+			}
+
+			return new Response(JSON.stringify(res), {
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+					"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+					"Content-Type": "application/json; charset=utf-8"
+				}
+			});
+			}
+
+			// キャッシュに保存する場合
+			if (reqBody.saveCache) {
+				// KVに保存
+			}
+
+			const res: ResBody = {
+				status: "S",
+				code: "000",
+				abstract: abstract,
+				useCache: false,
+				saveCache: false
+			}
+
+			return new Response(JSON.stringify(res), {
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+					"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+					"Content-Type": "application/json; charset=utf-8"
+				}
+			});
+		}
+
+		//-----------------------
+		// 要約を作成する関数
+		//
+		// 引数：
+		//	 reqBody: リクエストボディ
+		//	 env: 環境変数
+		// 戻り値：
+		//	 要約文字列
+		//-----------------------
+		async function CreateAbstract(reqBody:ReqBody, env:Env):Promise<string>{
+			// プロンプト作成
+			const pronpt = PronptBase + reqBody.entryBody;
+			// API呼び出しのための設定
+			const apiReq = {
+				contents: [{
+					parts: [
+						{
+							text: pronpt
+						}
+					]
+				}
+				]
+			};
+
+			const apiInit = {
+				body: JSON.stringify(apiReq),
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				}
+			}
+
+			const apiUrl = env.LLM_API_ENDPOINT + "?key=" + env.LLM_API_KEY;
+
+			// APIを呼び出す
+			const apiRes = await fetch(apiUrl, apiInit);
+
+			// APIエラーの場合ブランクを返す
+			if (apiRes.status !== 200) {
+				return "";
+			}
+			
+			const apiResBody:any = await apiRes.json();
+			const abstract:string = apiResBody.candidates[0].content.parts[0].text;
+			
+			return abstract;
+		}
 	},
+	
 } satisfies ExportedHandler<Env>;
